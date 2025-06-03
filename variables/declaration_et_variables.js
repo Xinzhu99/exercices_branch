f8eeb9c57e831588beff86880b0149e613d2afa6
Xinzhu99 <jxinzhu@gmail.com>
2025-06-03 12:18:47 +0200

    exercice variable fait

---
+++ variables/declaration_et_variables.js
@@ -0,0 +1,14 @@
+//Exo1
+let firstName = "Xinzhu";
+let lastName = "JIN";
+//vu que Alert ne s'affiche pas dans vscode, remplacé par console.log
+
+console.log(`Bonjour, ${lastName} ${firstName}`)
+
+
+//Exo 2
+const retirement = 64
+let myAGe = 32
+let workingYears = retirement - myAGe
+
+console.log(workingYears)
