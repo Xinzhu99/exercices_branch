76a2699eceb14d9aba023d5d2d28703767cd84e2
Xinzhu99 <jxinzhu@gmail.com>
2025-06-05 13:52:17 +0200

    ajout deuxième ex

--- functions/functions.js
+++ functions/functions.js
@@ -1,12 +1,16 @@
+//Exo1
 let client="Xinzhu";
-function makeTea (name){
+function makeTea (name, addSugar){
     console.log("Faire chauffer l'eau");
     console.log("Mettre du thé dans la thétière");
     console.log("Mettre l'eau dans la théière");
+    if (addSugar === true) {
+        console.log("Rajouter du sucre")
+    }
     console.log(`Servir le thé à ${name}`);
 }
 
-makeTea(client)
+makeTea(client, true)
 makeTea(1)
 
 
@@ -22,3 +26,15 @@ makeTea(1)
 // console.log("Rajouter du sucre")
 // console.log("Servir le thé à Samir");
 
+//Exo2
+function message(moment){
+    if (moment === "matin")
+    {console.log("J'ai trop envie de coder!")}
+    if (moment === "midi")
+    {console.log("J'ai faim, j'en ai marre des bugs...")}
+    if (moment === "après midi")
+    {console.log ("Tchao les copins!")}
+    if (moment === "soir")
+    {console.log("Zzzzzzzz")}
+};
+message("midi");
