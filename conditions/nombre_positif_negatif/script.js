d3784463e7d6612ed3f9c59994ac251380cb5be4
Xinzhu99 <jxinzhu@gmail.com>
2025-07-07 10:10:48 +0200

    first exercise

---
+++ conditions/nombre_positif_negatif/script.js
@@ -0,0 +1,17 @@
+/*
+Nous voulons notifier l’utilisateur si les nombres sont positifs ou négatifs.
+
+Ecris une condition qui affichera la nature du chiffre (positif, négatif…) dans la console.
+
+Déclare une variable qui contiendra un nombre et teste ta condition avec les valeurs suivantes que tu donneras à la variable : 6, -8 et 0.
+*/
+
+let number = 0;
+if (number > 0) {
+    console.log("positif");
+;}
+else if (number <0) {
+    console.log("négatif");
+} else {
+    console.log("zéro")};
+
