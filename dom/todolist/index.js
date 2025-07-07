fd9507041607d8bbbe00dc28d6924954f1cd754b
Xinzhu99 <jxinzhu@gmail.com>
2025-07-07 09:54:53 +0200

    rangement des dossiers

--- dom/todolist/index.js
+++ dom/todolist/index.js
@@ -1,31 +1,41 @@
-const task = document.querySelector("#todoTask")
+const todoTask = document.querySelector("#todoTask")
 const addButton = document.querySelector("#todoAdd")
-const list = document.querySelector("#todoList")
+const todoList = document.querySelector("#todoList")
+
 let myList = []
 
 addButton.addEventListener("click",() =>{
-    const taskLine = document.createElement("li")
-    const deleteButton = document.createElement("button")
-    myList.push(task.value)
-    taskLine.innerText=task.value
-    deleteButton.innerText="Supprimer"
-    taskLine.appendChild(deleteButton)
-    list.appendChild(taskLine)
+    myList.push(todoTask.value)
+    loadList()
 })
 
-deleteButton.addEventListener("click",() =>{
 
-})
-// function addItem(){
-//     todoList.push(task.value)
-//     refreshList()
-// }
-// function refreshList(){
-//     list.innerHTML = ''
-//     for(i in todoList){
-//         let element = document.createElement("li")
-//         element.innerHTML = todoList[i]
-//         list.appendChild(element)
+function loadList(){
+    todoList.innerHTML=''
+    //each deleteButton will need a unique id to know which element has to be deleted.
+    //initialize an index at 0 for the first button and increment the value at each iteration
+    let index = 0;
+    for (const item of myList){
+        todoList.innerHTML+=`<li>${item}<button id="${index}" class="deleteButton"> Supprimer</button></li>`
+        index++
+    }
+
+    const deleteButton = document.querySelectorAll(".deleteButton")
+    for (const item of deleteButton){
+        item.addEventListener("click", ()=> {
+            myList.pop(item.id)
+            loadList();
+        })
+    }
+
+    //Version alternative avec l'utilisation de la fonction deleteElement
+//     for (const item of deleteButton){
+//         item.addEventListener("click", deleteElement(item.id))
 //     }
-// }
-// addButton.addEventListener("click", addItem)
+}
+function deleteElement(id) {
+    return function(id){
+        myList.pop(id)
+        loadList();
+    }
+}
