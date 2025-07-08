09bf5d2aaf05c2fc0494f94626d911e7d93856c5
Xinzhu99 <jxinzhu@gmail.com>
2025-07-08 10:23:44 +0200

    exo compteur simple

---
+++ boucle/compteur/script.js
@@ -0,0 +1,33 @@
+/*
+Nous souhaitons créer des compteur.
+
+    Écris une boucle FOR qui affiche dans la console tous les nombres de 0 à 99.
+
+    Écris le même compteur avec une boucle WHILE.
+
+    Réflexion : Quel type de boucle te semble le plus approprié pour cette application ?
+
+    Ecris un compteur qui affiche tous les nombres de 50 à 99 (avec la boucle de ton choix).
+
+    Ecris un compteur qui affiche tous les nombre de 49 à 1 (avec la boucle de ton choix).
+
+*/
+// for (i=0; i<100; i++){
+//     console.log(i);
+// };
+
+// let n = 0;
+// while (n <100){
+//     console.log(n);
+//     n ++;
+// };
+
+let n = 50;
+while (n <= 99){
+    console.log(n);
+    n++;
+};
+
+for (i=49;i>0;i--){
+    console.log(i);
+};
