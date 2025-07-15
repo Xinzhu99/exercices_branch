8b90b59c105148cf339f6f72bb8d5816666e7ad6
Xinzhu99 <jxinzhu@gmail.com>
2025-07-15 15:18:35 +0200

    first exo added

---
+++ async/fetch_offres/script.js
@@ -0,0 +1,18 @@
+async function fetchOffers(){
+    const response = await fetch("https://api.allorigins.win/raw?url=https://codepassport.dev/api/offers");
+    // j'ai collé https://api.allorigins.win/raw?url= avant mon url d'api car erreur corps
+    const offers = await response.json();
+
+    const offerList = document.querySelector("#offerList");
+
+
+    for (const offer of offers){
+        const title = document.createElement("h2");
+        const jd = document.createElement("p");
+        title.innerHTML = offer.titre;
+        jd.innerHTML = offer.description;
+        offerList.appendChild(title);
+        offerList.appendChild(jd);
+    }
+}
+fetchOffers();
