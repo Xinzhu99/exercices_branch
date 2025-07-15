f4c885e85ebaa435d4787e6d0aca6eb481f9b3d5
Xinzhu99 <jxinzhu@gmail.com>
2025-07-15 10:18:03 +0200

    exo 1 ajouté

---
+++ manip_donnees/parcourir_un_tableau/parcourir_un_tableau/parcourir_un_tableau.js
@@ -0,0 +1,12 @@
+const candidates = ['Le Filip','Ruby On The Nail','Leona Winter','Lula Strega','Misty Phoenix',
+  'Perseo','Norma Bell','Edeha Noire','Magnetica','Afrodite Amour'];
+
+const list = document.querySelector("#list")
+console.log(candidates[0])
+console.log(candidates[7])
+
+
+for (const candidate of candidates){
+    list.innerHTML += `<li>${candidate}</li>`
+}
+
