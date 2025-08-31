6bfc5f23ce7059bb38481f7e64044b141d78420f
Xinzhu99 <jxinzhu@gmail.com>
2025-08-31 11:44:21 +0200

    regex mail verification fonctional

--- responsive/reproduction/script.js
+++ responsive/reproduction/script.js
@@ -1,18 +1,24 @@
 const btn = document.querySelector("#btn");
 const input = document.querySelector("#userInput");
 const errorMsg = document.querySelector("#errorMsg");
-const errorImg=document.querySelector("#errorImg");
+const errorImg = document.querySelector("#errorImg");
 
-// console.log("btn",btn)
-// console.log("input",input.value)
 
-input.value="";
+input.value = "";
 
-btn.addEventListener("click",()=>{
-    const email = input.value.trim();
-    let result = email.includes("@");
-    console.log("input",result);
-    if(result===false){
+function validationEmail(email){
+    console.log("email",email)
+    const emailPattern = /^[a-zA-Z0–9._%+-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+
+}
+
+btn.addEventListener("click", () => {
+    var email = input.value.trim();
+
+    if (validationEmail(email)) {
+        console.log("Valid email address.");
+    } else {
         input.style.border = "2px solid red";
         errorMsg.style.display = "block";
         errorImg.style.display = "block";
